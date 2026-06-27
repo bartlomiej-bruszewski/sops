5258addbf494af3c76da905d45723a0381f3db7b
Bartłomiej Bruszewski <bruszewski.bartek12@gmail.com>
2026-06-27 19:47:04 +0200

    Poprawki

--- shamir/sharing/utils.go
+++ shamir/sharing/utils.go
@@ -10,16 +10,17 @@ var Prime = big.NewInt(997)
 
 // FieldAdd powinno zwrócić (a + b) mod Prime
 func FieldAdd(a, b *big.Int) *big.Int {
-	res := new(big.Int).add(a,b)
+	res := new(big.Int).Add(a,b)
 	return res.Mod(res, Prime)
 }
 
 // FieldSub powinno zwrócić (a - b) mod Prime
 // Pamiętaj, że jeśli wynik odejmowania jest ujemny, musisz dodać Prime!
 func FieldSub(a, b *big.Int) *big.Int {
-	res := new(big.Int).sub(a,b)
-	if res<0:
-		res=res+Prime
+	res := new(big.Int).Sub(a,b)
+	if res.Sign()<0{
+		res=res.Add(res, Prime)
+	}
 	return res.Mod(res, Prime)
 	// TWOJE ZADANIE
 }
