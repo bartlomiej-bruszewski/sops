131785aa400023b7554118f695dedd6db0c6ae91
Bartłomiej Bruszewski <bruszewski.bartek12@gmail.com>
2026-06-27 20:07:50 +0200

    Chunker v2

--- shamir/sharing/chunker.go
+++ shamir/sharing/chunker.go
@@ -8,12 +8,22 @@ import (
 // Tutaj napisz swoją funkcję BytesToBigInts od zera!
 func BytesToBigInts(data []byte) ([]*big.Int, error){
 	if len(data)==0{
-	return nil, errors.New("Empty data")
+		return nil, errors.New("Empty data")
 	}
 	result := make([]*big.Int, len(data))
-	for i, b in range data{
-		big.NewInt(int64(b))
-		result[i]=big
+	for i, b := range data{
+		result[i] = big.NewInt(int64(b))
 	}
-	return result
+	return result, nil
+}
+
+func BigIntsToBytes(elements []*big.Int) ([]*byte, error){
+	if len(elements)==0{
+		return nil, errors.New("Empty elements")
+	}
+	result =: make([]*byte, len(data))
+	for i, B := range elements{
+		result[i] = byte(elem.Uint64())
+	}
+	return result, nil
 }
