4233fc405d78a0ca53448a3a1f2a3ae01a6475cc
Bartłomiej Bruszewski <bruszewski.bartek12@gmail.com>
2026-06-27 19:53:13 +0200

    Poprawki, mnożenie i dzielenie

--- shamir/sharing/utils.go
+++ shamir/sharing/utils.go
@@ -2,6 +2,7 @@ package sharing
 
 import (
 	"math/big"
+	"errors"
 )
 
 // Nasz moduł p (liczba pierwsza) - dla ułatwienia zdefiniujmy globalną zmienną.
@@ -19,7 +20,7 @@ func FieldAdd(a, b *big.Int) *big.Int {
 func FieldSub(a, b *big.Int) *big.Int {
 	res := new(big.Int).Sub(a,b)
 	if res.Sign()<0{
-		res=res.Add(res, Prime)
+		res.Add(res, Prime)
 	}
 	return res.Mod(res, Prime)
 	// TWOJE ZADANIE
@@ -27,3 +28,19 @@ func FieldSub(a, b *big.Int) *big.Int {
 
 Spróbuj napisać te dwie funkcje. Nie przejmuj się, jeśli coś pójdzie nie tak z obsługą wskaźników w `math/big` – skorygujemy to i wyjaśnię Ci, jak Go zarządza tą pamięcią!
 
+// FieldMult powinno zwrócić (a * b) mod Prime.
+func FieldMult(a, b *big.Int) *big.Int {
+	res := new(big.Int).Mul(a,b)
+	return res.Mod(res, Prime)
+}
+
+// FieldDiv powinno zwrócić (a / b) mod Prime, czyli (a * b^-1) mod Prime.
+// Jeśli b jest równe 0, funkcja powinna zwrócić błąd: errors.New("division by zero")
+func FieldDiv(a, b *big.Int) (*big.Int, error) {
+	inv := new(big.Int).ModInverse(b, Prime)
+	if inv == nil{
+		errors.New("division by zero")
+	}
+	res := new(big.Int).Mul(a,b)
+	return res.Mod(res, Prime), nil
+}
