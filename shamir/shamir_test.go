7b9aee6c8c6830230bd3cea5b312cf9213b810ff
Bartłomiej Bruszewski <bruszewski.bartek12@gmail.com>
2026-06-27 21:13:11 +0200

    test: add unit test for prepareLagrangeBasis

--- shamir/shamir_test.go
+++ shamir/shamir_test.go
@@ -215,13 +215,13 @@ func TestInterpolate_Rand(t *testing.T) {
 
 func TestPrepareLagrangeBasis(t *testing.T){
 	for i:=0; i<256; i++{
-		p, err = makePolynomial(uint8(i), 2)
+		p, err := makePolynomial(uint8(i), 2)
 		if err!=nil{
 			t.Fatalf("err %v", err)
 		}
 		xSamples := []uint8{1,2,3}
 		ySamples := []uint8{p.evaluate(1), p.evaluate(2), p.evaluate(3)}
-		expextedSecret := interpolatePolynomial(xSamples, ySamples, 0)
+		expectedSecret := interpolatePolynomial(xSamples, ySamples, 0)
 
 		basisFactors := prepareLagrangeBasis(xSamples)
 		if len(basisFactors) != len(xSamples){
@@ -229,7 +229,7 @@ func TestPrepareLagrangeBasis(t *testing.T){
 		}
 		var actualSectet uint8
 		for idx, y := range ySamples{
-			group := mult(idx, y)
+			group := mult(basisFactors[idx], y)
 			actualSecret = add(actualSecret, group)
 		}
 		if actualSecret!=expectedSecret{
